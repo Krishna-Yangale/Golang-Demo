46982440ca8471c012196178555a8e84154f5ebf
Krishna-Yangale <krishna.yangale@radiant.co>
2022-06-09 12:53:36 +0530

    Fucntion program to convert dollar into rupees

---
+++ function.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// rupees := convertDollar(54)
+	// fmt.Println(rupees)
+
+	fmt.Println(convertDollar(20))
+
+}
+
+func convertDollar(dollar float64) float64 {
+	return dollar * 72
+}
