18ea613299b80c36dc3c08af36e174a0c6328e40
Krishna-Yangale <krishna.yangale@radiant.co>
2022-06-09 13:00:50 +0530

    ifelse program in go

---
+++ ifelse.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// number := 9
+
+	// if number%2 == 0 {
+	// 	fmt.Println("Number is even number")
+	// } else {
+	// 	fmt.Println("The number is odd number")
+	// }
+
+	if number := 10; number%2 == 0 {
+		fmt.Println("Number is even number")
+	} else {
+		fmt.Println("number is odd number")
+	}
+}
